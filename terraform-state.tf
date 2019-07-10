8a07a43e32c3ebf47f55b4c1dc664ae6dfa265a9
Administrator <jagath@jdplc.com>
2019-07-11 00:14:48 +0530

    deleted tfstate as it was not required for this module

--- terraform-state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-backend "s3" {
-encrypt = true
-bucket = "terraform-state-store-ireland"
-#dynamodb_table = "terraform-state-lock-dynamo"
-region = "eu-west-1"
-key = "jd-modules-vpc-k8s-state-s3.tfstate"
-}
-}
